17abc786bcd6741d5f0fa498051f7b76d151e8e3
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-05-21 21:41:29 -0300

    feat: initial commmit

--- aula-spring/build.gradle.kts
+++ aula-spring/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-	java
 	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.23"
+	kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "com.swxtz"
@@ -23,10 +26,19 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "21"
+	}
 }
 
 tasks.withType<Test> {
---
+++ aula-spring/src/main/kotlin/com/swxtz/aulaspring/AulaSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.swxtz.aulaspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AulaSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<AulaSpringApplication>(*args)
+}
